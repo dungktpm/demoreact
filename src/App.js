a91d4b844890166cbc0dd78fc4083ebe21bf445b
webappenv <quangdung@quangich.com>
2022-02-16 10:21:47 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Chào mừng các mem
         </p>
         <a
           className="App-link"
